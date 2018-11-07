edbf8409c713eedb674203049bb77beffbcdce44
3A532051 <chu123520@gmail.com>
2018-11-07 16:18:07 +0800

    ch7 實作練習前準備

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Route;
+
 class BoardController extends Controller
 {
-    //
+    public function getName(){
+        return"排行榜";
+    }
+
+    public function getName(){
+        return Route::currentRouteAction();
+    }
 }
