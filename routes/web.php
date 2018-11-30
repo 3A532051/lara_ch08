57be3c273ac1442e4c5d0f626ac85330679bb060
3A532051 <chu123520@gmail.com>
2018-11-30 15:15:28 +0800

    測試並修正路由、超連結及按鈕

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::group(['namespace'=>'Cool'],function() {
     Route::get('cool', 'TestController@index');});
 
-Route::pattern('student_no','s[0-9]{10}');
+//Route::pattern('student_no','s[0-9]{10}');
 
 Route::group(['prefix'=>'student'],function(){
 
